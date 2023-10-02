b292ce077237bf5b68f9cc3649aba052c2d1bc98
EljayiYassir <yassireljay@gmail.com>
2023-10-02 17:55:36 +0100

    prediction pipeline with flask app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,7 +8,6 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
-from sklearn.model_selection import RandomizedSearchCV # for hyperparameters
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 
@@ -54,9 +53,55 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params={
+                "Linear Regression":{},
+
+                "Gradient Boosting Regression":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [2,3,4,5,6], 
+                    'weights': ['uniform','distance']
+                },
+
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
             models_report:dict=evaluate_models(X_train=X_train, y_train=y_train, 
                                                X_test=X_test, y_test=y_test, 
-                                               models=models)
+                                               models=models, params=params)
             best_model_score=max( models_report.values())
             best_model_name=list(models_report.keys())[
                 list(models_report.values()).index(best_model_score)
@@ -76,7 +121,7 @@ class ModelTrainer:
 
             predicted=best_model.predict(X_test)
             score=r2_score(y_test,predicted)
-            return best_model_name, score
+            return score
 
         except Exception as e:
             raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,10 @@ import numpy as np
 import pandas as pd
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV 
 
 import dill
+import pickle
 
 from src.exception import CustomException
 from src.logger import logging
@@ -22,12 +24,12 @@ def save_object(file_path, obj) -> None:
         os.makedirs(dir_path, exist_ok=True)
         
         with open(file_path,"wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models) -> dict:
+def evaluate_models(X_train, y_train, X_test, y_test, models, params) -> dict:
     '''
     Function that return report of the evaluation of all the models performance
 
@@ -37,9 +39,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models) -> dict:
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = list(params.values())[i]
+            
 
+            gs = GridSearchCV(model,param,cv=4)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
-            y_train_pred= model.predict(X_train)
+            # y_train_pred= model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
             # train_model_score=r2_score(y_train,y_train_pred)
@@ -49,3 +57,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models) -> dict:
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys) 
